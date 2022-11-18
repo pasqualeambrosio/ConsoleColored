53a7a85543c41a64ea9ffe67d5f389996149c7e8
Pasquale Ambrosio <ambrosio.pasquale@gmail.com>
2022-11-18 14:56:03 +0100

    Add files via upload

---
+++ testConsoleColored/testConsoleColored/CConsole.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CConsole
+{
+    private static ConsoleColor defaultFontColor = ConsoleColor.Gray;
+    private static ConsoleColor defaultBackgroundColor = ConsoleColor.Black;
+    private static bool defaultColorsChanged = false;
+
+    public enum Colors
+    {
+        Default,
+        Muted,
+        Secondary,
+        Info,
+        Warning,
+        Error,
+        BgDefault,
+        BgMuted,
+        BgInfo,
+        BgWarning,
+        BgError,
+    }
+
+    #region WriteLine / Write
+
+    public static void WriteLine(string value, Colors colors)
+    {
+        WriteOnConsole(value, true, colors);
+    }
+
+    public static void Write(string value, Colors colors)
+    {
+        WriteOnConsole(value, false, colors);
+    }
+
+    public static void WriteLine(string value, ConsoleColor fontColor = ConsoleColor.Gray, ConsoleColor backgroundColor = ConsoleColor.Black)
+    {
+        WriteOnConsole(value, true, fontColor, backgroundColor);
+    }
+
+    public static void Write(string value, ConsoleColor fontColor = ConsoleColor.Gray, ConsoleColor backgroundColor = ConsoleColor.Black)
+    {
+        WriteOnConsole(value, false, fontColor, backgroundColor);
+    }
+
+    #endregion
+
+    #region SetColors / ResetColors
+
+    public static void SetColors(ConsoleColor fontColor = ConsoleColor.Gray, ConsoleColor backgroundColor = ConsoleColor.Black)
+    {
+        defaultColorsChanged = true;
+        defaultFontColor = fontColor;
+        defaultBackgroundColor = backgroundColor;
+        Console.ForegroundColor = defaultFontColor;
+        Console.BackgroundColor = defaultBackgroundColor;
+    }
+
+    public static void SetColors(Colors colors)
+    {
+        defaultColorsChanged = true;
+        defaultFontColor = GetColors(colors)[0];
+        defaultBackgroundColor = GetColors(colors)[1];
+        Console.ForegroundColor = defaultFontColor;
+        Console.BackgroundColor = defaultBackgroundColor;
+    }
+
+    public static void ResetColors()
+    {
+        defaultColorsChanged = false;
+        defaultFontColor = ConsoleColor.Gray;
+        defaultBackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = defaultFontColor;
+        Console.BackgroundColor = defaultBackgroundColor;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private static void WriteOnConsole(string value, bool withReturn, ConsoleColor fontColor = ConsoleColor.Gray, ConsoleColor backgroundColor = ConsoleColor.Black)
+    {
+        CheckDefaultColors(fontColor, backgroundColor);
+
+        if (withReturn)
+            Console.WriteLine(value);
+        else
+            Console.Write(value);
+
+        Console.ForegroundColor = defaultFontColor;
+        Console.BackgroundColor = defaultBackgroundColor;
+    }
+
+    private static void WriteOnConsole(string value, bool withReturn, Colors color)
+    {
+        CheckDefaultColors(color);
+
+        if (withReturn)
+            Console.WriteLine(value);
+        else
+            Console.Write(value);
+
+        Console.ForegroundColor = defaultFontColor;
+        Console.BackgroundColor = defaultBackgroundColor;
+    }
+
+    private static void CheckDefaultColors(ConsoleColor fontColor, ConsoleColor backgroundColor)
+    {
+        if (!defaultColorsChanged)
+        {
+            Console.ForegroundColor = fontColor;
+            Console.BackgroundColor = backgroundColor;
+        }
+    }
+
+    private static void CheckDefaultColors(Colors color)
+    {
+        if (!defaultColorsChanged)
+        {
+            Console.ForegroundColor = GetColors(color)[0];
+            Console.BackgroundColor = GetColors(color)[1];
+        }
+    }
+
+    private static ConsoleColor[] GetColors(Colors colors)
+    {
+        ConsoleColor[] res = new ConsoleColor[] { ConsoleColor.Gray, ConsoleColor.Black };
+
+        switch (colors)
+        {
+            #region Without Background
+
+            case Colors.Default:
+                res[0] = ConsoleColor.Gray;
+                break;
+
+            case Colors.Muted:
+                res[0] = ConsoleColor.DarkGray;
+                break;
+
+            case Colors.Info:
+                res[0] = ConsoleColor.Cyan;
+                break;
+
+            case Colors.Warning:
+                res[0] = ConsoleColor.Yellow;
+                break;
+
+            case Colors.Error:
+                res[0] = ConsoleColor.DarkRed;
+                break;
+
+            #endregion
+
+            #region With Background
+
+            case Colors.BgDefault:
+                res[0] = ConsoleColor.DarkGray;
+                res[1] = ConsoleColor.Gray;
+                break;
+
+            case Colors.BgMuted:
+                res[0] = ConsoleColor.White;
+                res[1] = ConsoleColor.Gray;
+                break;
+
+            case Colors.BgInfo:
+                res[0] = ConsoleColor.White;
+                res[1] = ConsoleColor.Cyan;
+                break;
+
+            case Colors.BgWarning:
+                res[0] = ConsoleColor.Black;
+                res[1] = ConsoleColor.Yellow;
+                break;
+
+            case Colors.BgError:
+                res[0] = ConsoleColor.White;
+                res[1] = ConsoleColor.DarkRed;
+                break;
+
+                #endregion
+        }
+
+        return res;
+    }
+
+    #endregion
+
+
+}
+
---
+++ testConsoleColored/testConsoleColored/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsoleColored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CConsole.WriteLine("Default", CConsole.Colors.Default);
+            CConsole.WriteLine("Muted", CConsole.Colors.Muted);
+            CConsole.WriteLine("Info", CConsole.Colors.Info);
+            CConsole.WriteLine("Warning", CConsole.Colors.Warning);
+            CConsole.WriteLine("Error", CConsole.Colors.Error);
+
+            CConsole.WriteLine("BgDefault", CConsole.Colors.BgDefault);
+            CConsole.WriteLine("BgMuted", CConsole.Colors.BgMuted);
+            CConsole.WriteLine("BgInfo", CConsole.Colors.BgInfo);
+            CConsole.WriteLine("BgWarning", CConsole.Colors.BgWarning);
+            CConsole.WriteLine("BgError", CConsole.Colors.BgError);
+
+            CConsole.Write("Hello ", CConsole.Colors.Error);
+            CConsole.WriteLine("World", CConsole.Colors.BgError);
+
+
+            Console.ReadKey();
+
+
+            CConsole.WriteLine("test1", ConsoleColor.DarkBlue);
+
+            CConsole.Write("Hello ", ConsoleColor.Blue);
+            CConsole.WriteLine("World", ConsoleColor.Green);
+            CConsole.WriteLine("test2");
+            CConsole.WriteLine(string.Format("test {0}", 3), ConsoleColor.Red, ConsoleColor.Green);
+
+            CConsole.SetColors(ConsoleColor.Red);
+
+            CConsole.WriteLine("test4");
+            CConsole.WriteLine("test5");
+
+            CConsole.ResetColors();
+
+            CConsole.WriteLine("test6", ConsoleColor.Blue);
+            CConsole.WriteLine("test7");
+
+            Console.ReadKey();
+        }
+    }
+}
